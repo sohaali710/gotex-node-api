2b3528c7362ef02c563e4746692b2ff8fa65c44b
amer200 <amermostaafa@gmail.com>
2023-09-14 04:05:37 +0300

    commit

--- routes/spl.js
+++ routes/spl.js
@@ -9,7 +9,7 @@ const { createNewOrder, getCities, getToken, getUserOrders, getCountries, getDis
 routes.post("/create-new-order", isValid, splCheck, createNewOrder); //done
 routes.post("/get-cities", isValid, getCities); //done
 routes.get("/token", getToken); // don't share this in doc 
-routes.post("/get-all-orders", isValid, getUserOrders); // not added to doc  //done
+routes.post("/get-user-orders", isValid, getUserOrders); // not added to doc  //done
 routes.get("/get-countries", getCountries); // note used 
 routes.post("/get-districts", getDistrict); // note used 
 
