8121a7f10fa4d5caa47690c6138ffe721392f9ac
Soha <sohaali710@gmail.com>
2023-10-25 22:18:28 +0300

    Create order

--- controller/saee.js
+++ controller/saee.js
@@ -3,8 +3,8 @@ const User = require("../model/user");
 const Saee = require("../model/companies/saee");
 const SaeeOrder = require("../model/orders/saeeOrders");
 
-/** pickup shipment */
 exports.createUserOrder = async (req, res) => {
+    const pickup = req.query.pickup || false // by default pickup=false (last mile order)
     const {
         p_name, p_city, p_mobile, p_streetaddress,
         c_name, c_city, c_mobile, c_streetaddress,
@@ -18,95 +18,51 @@ exports.createUserOrder = async (req, res) => {
 
     try {
         const paytype = cod ? "cod" : "cc";
+        let data = {}, url = '';
+        if (pickup) {
+            url = 'https://k-w-h.com/deliveryrequest/newpickup'
+            data = {
+                secret: process.env.SAEE_KEY_P,
+                cashonpickup: 0,
+                cashondelivery,
 
-        const data = {
-            secret: process.env.SAEE_KEY_P,
-            cashonpickup: 0,
-            cashondelivery,
+                p_name, p_city, p_mobile, p_streetaddress,
+                c_name, c_city, c_mobile, c_streetaddress,
+                weight, quantity, description, cod,
 
-            p_name, p_city, p_mobile, p_streetaddress,
-            c_name, c_city, c_mobile, c_streetaddress,
-            weight, quantity, description, cod,
+                ordernumber: `${ordersNum + "/" + Date.now() + "gotex"}`,
+                sendername: p_name,
+                senderphone: p_mobile,
+                senderaddress: p_streetaddress,
+                sendercity: p_city,
+                // sendercountry: "SA"
+            }
+        } else {
+            url = 'https://k-w-h.com/deliveryrequest/new'
+            data = {
+                secret: process.env.SAEE_KEY_P,
+                cashonpickup: 0,
+                cashondelivery,
 
-            ordernumber: `${ordersNum + "/" + Date.now() + "gotex"}`,
-            sendername: p_name,
-            senderphone: p_mobile,
-            senderaddress: p_streetaddress,
-            sendercity: p_city,
-            // sendercountry: "SA"
+                name: p_name,
+                city: p_city,
+                mobile: p_mobile,
+                streetaddress: p_streetaddress,
+                weight, quantity, description, cod,
+
+                ordernumber: `${ordersNum + "/" + Date.now() + "gotex"}`,
+                sendername: p_name,
+                senderphone: p_mobile,
+                senderaddress: p_streetaddress,
+                sendercity: p_city,
+                // sendercountry: "SA"
+            }
         }
 
         const config = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json;charset=utf-8' },
-            url: 'https://k-w-h.com/deliveryrequest/newpickup',
-            data: data
-        }
-        const response = await axios(config)
-
-        const order = await SaeeOrder.create({
-            user: userId,
-            company: "saee",
-            ordernumber: `${ordersNum + "/" + Date.now() + "gotex"}`,
-            data: response.data,
-            paytype,
-            price: totalShipPrice,
-            createdate: new Date()
-        })
-
-        if (!response.data.success) {
-            order.status = 'failed'
-            await order.save()
-            return res.status(400).json({ msg: response.data })
-        }
-
-        if (!cod) {
-            user.wallet = user.wallet - totalShipPrice
-            await user.save()
-        }
-
-        res.status(200).json({ msg: "order created", data: order })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            error: err.message
-        })
-    }
-}
-exports.createOrderLastMile = async (req, res) => {
-    const {
-        name, city, mobile, streetaddress,
-        weight, quantity, cod, description, userId } = req.body
-
-    const totalShipPrice = res.locals.totalShipPrice;
-    const cashondelivery = res.locals.codAmount;
-
-    const user = await User.findById(userId);
-    let ordersNum = await SaeeOrder.count();
-
-    try {
-        const paytype = cod ? "cod" : "cc";
-
-        const data = {
-            secret: process.env.SAEE_KEY_P,
-            cashonpickup: 0,
-            cashondelivery,
-
-            name, city, mobile, streetaddress,
-            weight, quantity, description, cod,
-
-            ordernumber: `${ordersNum + "/" + Date.now() + "gotex"}`,
-            sendername: name,
-            senderphone: mobile,
-            senderaddress: streetaddress,
-            sendercity: city,
-            // sendercountry: "SA"
-        }
-
-        const config = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json;charset=utf-8' },
-            url: 'https://k-w-h.com/deliveryrequest/new',
+            url: url,
             data: data
         }
         const response = await axios(config)
--- routes/saee.js
+++ routes/saee.js
@@ -3,11 +3,11 @@ const routes = express.Router();
 const Saee = require("../model/companies/saee");
 const { checkCompany } = require("../middleware/company");
 const { isAdminAuth } = require("../middleware/admin");
-const { createUserOrder, getSticker, edit, getUserOrders, trackingOrderByNum, getCities, cancelOrder, createOrderLastMile } = require("../controller/saee");
+const { createUserOrder, getSticker, edit, getUserOrders, trackingOrderByNum, getCities, cancelOrder } = require("../controller/saee");
 const { isValid } = require("../middleware/api-test");
 
 routes.post("/create-user-order", isValid, checkCompany(Saee), createUserOrder);
-routes.post("/create-order-last-mile", isValid, checkCompany(Saee), createOrderLastMile);
+// routes.post("/create-order-last-mile", isValid, checkCompany(Saee), createOrderLastMile);
 routes.post("/print-sticker/:id", isValid, getSticker);
 routes.post("/cancel-order", isValid, cancelOrder);
 routes.post("/track-order-by-number", isValid, trackingOrderByNum);
