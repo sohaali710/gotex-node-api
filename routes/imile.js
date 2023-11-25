0bb917f0f62d0565a017dbe04d656dc84c4c333b
Soha <sohaali710@gmail.com>
2023-11-26 00:29:25 +0200

    Update

--- routes/imile.js
+++ routes/imile.js
@@ -4,12 +4,12 @@ const Imile = require("../model/companies/imile");
 const { checkCompany } = require("../middleware/company");
 const { isAdminAuth } = require("../middleware/admin");
 const { isValid } = require("../middleware/api-test");
-const { createOrder, addClient, getSticker, cancelOrder, getAllClients, getUserOrders, edit, getStickerTest } = require("../controller/imile");
+const { createOrder, addClient, getSticker, cancelOrder, getAllClients, getUserOrders, edit } = require("../controller/imile");
 
 routes.post("/create-user-order", isValid, checkCompany(Imile), createOrder);
 routes.post("/add-client", isValid, addClient);
 routes.post("/get-all-clients", isValid, getAllClients);
-routes.post("/print-sticker/:id", isValid, getSticker); ks
+routes.post("/print-sticker/:id", isValid, getSticker);
 routes.post("/cancel-order", isValid, cancelOrder);
 
 routes.post("/get-user-orders", isValid, getUserOrders);
